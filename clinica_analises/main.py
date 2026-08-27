fda15874d60a0cd31a95e0d30b137f43c25a0614
Pablo <fp902405@gmail.com>
2026-08-26 22:39:59 -0300

    fazendo configuração basica e testes

--- clinica_analises/main.py
+++ clinica_analises/main.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
 
+@app.route('/')
+def loguin():
+    return render_template('login.html')
+
+
+@app.route('/dashboard')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__' : 
     with app.app_context(): 
         db.create_all()
