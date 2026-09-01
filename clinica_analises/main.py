050bb83c621bd9410e4fc6ac7366f20de69a6ad6
Pablo <fp902405@gmail.com>
2026-09-01 00:17:27 -0300

    concertando erros

--- clinica_analises/main.py
+++ clinica_analises/main.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request , redirect, url_for, flash, session
+from datetime import datetime
 from db import db
 
 app = Flask(__name__)
