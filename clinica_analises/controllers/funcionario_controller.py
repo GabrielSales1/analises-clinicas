41bfc596e122cf056e459da45e51a397c84a48b2
Pablo <fp902405@gmail.com>
2026-09-12 16:04:13 -0300

    refatorando usuarios e padronizando o seu front end

---
+++ clinica_analises/controllers/funcionario_controller.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template, request , redirect, url_for, flash, session
+import flask_login
+from db import db
+from main import app
+
+from models.Usuario_model import Usuario
+from models.Paciente_model import Paciente
+from models.Funcionario_model import Funcionario
+
+
+@app.route('/funcionario/listar')
+@flask_login.login_required
+def funcionario_listar() : 
+    funcionarios = Funcionario.query.all()
+    return render_template('paciente/lista_pacientes.html',user=flask_login.current_user,funcionarios = funcionarios)
